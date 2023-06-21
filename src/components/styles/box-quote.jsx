707d533380d50f354e237fc0aa51b5aad9381c1f
Mamadou <sallmahdou1@gmail.com>
2023-06-21 23:42:29 +0200

    fix material ui dependancy

--- src/components/styles/box-quote.jsx
+++ src/components/styles/box-quote.jsx
@@ -1,4 +1,4 @@
-import { makeStyles } from "@mui/material/styles";
+import { makeStyles } from "@mui/styles";
 import { Colors } from "./theme.jsx";
 
 export const useStyles = makeStyles((theme) => ({
